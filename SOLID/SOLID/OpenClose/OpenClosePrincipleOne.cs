5e5d3a1e69b63131efd558c422899a15b2f59302
Rachit Srivastava <rachitsrivastava77@gmail.com>
2022-03-14 00:02:07 +0530

    Liskov and OpenClose

---
+++ SOLID/SOLID/OpenClose/OpenClosePrincipleOne.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.OpenClose
+{
+    public class Database
+    {
+        public void AddCustomer()
+        {
+
+        }
+
+        public void AddExistingCustomer()
+        {
+
+        }
+
+        public void AddAnotherTypeCustomer()
+        {
+
+        }
+    }
+
+    public class OpenClosePrincipleOneNonCompliant
+    {
+        public class Customer
+        {
+            int Type;
+
+            /*
+                Currently there are 2 types of Customers New Customers and ExistingCustomers, and if we want to add 
+                another type of Cusomter, we will need to add another if else condition which will modify the existing code.
+                Hence, violating this principle.
+             */
+            public void AddCustomer(Database db)
+            {
+                if (Type == 0)
+                {
+                    // Add New Customer
+                    db.AddCustomer();
+                }
+                else
+                {
+                    db.AddExistingCustomer();
+                }
+            }
+        }
+    }
+
+    public class OpenClosePrincipleOne
+    {
+        public class Customer
+        {
+            public virtual void AddCustomer(Database db)
+            {
+                // Add New Customer
+                db.AddCustomer();
+            }
+        }
+
+        public class ExistingCustomer : Customer
+        {
+            public override void AddCustomer(Database db)
+            {
+                db.AddExistingCustomer();
+            }
+        }
+
+        public class AnotherTypeCustomer : Customer
+        {
+            public override void AddCustomer(Database db)
+            {
+                db.AddAnotherTypeCustomer();
+            }
+        }
+    }
+
+    /*
+     * Another example can be Report Generation with type Crystal Report, PDF Report, PowerPointReport etc.
+     */
+}
---
+++ SOLID/SOLID/OpenClose/OpenClosePrincipleTwo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.OpenClose
+{
+    public class DatabaseOpenClose
+    {
+        public void Add(string postMessage)
+        {
+
+        }
+
+        public void AddAsTag(string postMessage)
+        {
+
+        }
+
+        public void AddAsHashTag(string postMessage)
+        {
+
+        }
+    }
+
+    class OpenClosePrincipleTwoNonCompliant
+    {
+        public class Post
+        {
+            public void CreatePost(DatabaseOpenClose db, string postMessage)
+            {
+                if (postMessage.StartsWith("@"))
+                {
+                    db.AddAsTag(postMessage);
+                }
+                // If another message that starts with '#' comes, then another if else condition needs to be added.
+                // The class will need to be heavily modified to incorporate changing requirements.
+                else if (postMessage.StartsWith("#"))
+                {
+                    db.AddAsHashTag(postMessage);
+                }
+                else
+                {
+                    db.Add(postMessage);
+                }
+            }
+        }
+    }
+
+    class OpenClosePrincipleTwo
+    {
+        /*
+         * By using Inheritance, it is much easier to create extended behaviour to the Post object by oerriding the CreatePost() method.
+         */
+        public class Post
+        {
+            public virtual void CreatePost(DatabaseOpenClose db, string postMessage)
+            {
+                db.Add(postMessage);
+            }
+        }
+
+        public class TagPost : Post
+        {
+            public override void CreatePost(DatabaseOpenClose db, string postMessage)
+            {
+                db.AddAsTag(postMessage);
+            }
+        }
+
+        public class HashTagPost : Post
+        {
+            public override void CreatePost(DatabaseOpenClose db, string postMessage)
+            {
+                db.AddAsHashTag(postMessage);
+            }
+        }
+    }
+}
